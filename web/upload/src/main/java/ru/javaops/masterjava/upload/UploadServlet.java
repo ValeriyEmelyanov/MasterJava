45c256c0971fe87872526add9226f9c2007a9712
Valeriy Emelyanov <emelva@rambler.ru>
2023-08-04 13:02:41 +0300

    Save users with DBI

--- web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
@@ -1,5 +1,6 @@
 package ru.javaops.masterjava.upload;
 
+import com.google.common.collect.ImmutableMap;
 import org.thymeleaf.context.WebContext;
 import ru.javaops.masterjava.persist.model.User;
 
@@ -12,19 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
 @WebServlet(urlPatterns = "/", loadOnStartup = 1)
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10) //10 MB in memory limit
 public class UploadServlet extends HttpServlet {
+    private static final int CHUNK_SIZE = 1000;
 
     private final UserProcessor userProcessor = new UserProcessor();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
+        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
+                ImmutableMap.of("Message", "", "chunkSize", CHUNK_SIZE));
         engine.process("upload", webContext, resp.getWriter());
     }
 
@@ -32,6 +37,8 @@ public class UploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
 
+        String message = "";
+        int chunkSize = CHUNK_SIZE;
         try {
 //            http://docs.oracle.com/javaee/6/tutorial/doc/glraq.html
             Part filePart = req.getPart("fileToUpload");
@@ -39,7 +46,8 @@ public class UploadServlet extends HttpServlet {
                 throw new IllegalStateException("Upload file have not been selected");
             }
             try (InputStream is = filePart.getInputStream()) {
-                List<User> users = userProcessor.process(is);
+                chunkSize = Integer.parseInt(req.getParameter("chunkSize"));
+                List<User> users = userProcessor.process(is, chunkSize);
                 webContext.setVariable("users", users);
                 engine.process("result", webContext, resp.getWriter());
             }
